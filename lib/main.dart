5fbedcf84e89cd54dba99063a383c7f3e28ff288
Majok-Bol <bolmajok001@gmail.com>
2025-03-05 14:41:20 +0300

    changed state to stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,19 +2,53 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+  void answerQuestion() {
+    setState(() {
+      questionIndex = questionIndex + 1;
+    });
+
+    print(questionIndex);
+  }
+
+  var questions = [
+    'What is your favorite colour?',
+    'What is your favorite animal?',
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text('my first app')),
+        appBar: AppBar(title: Text(questions[questionIndex])),
         body: Column(
           children: <Widget>[
             Text('Questions'),
-            ElevatedButton(onPressed: null, child: Text('answer one')),
-            ElevatedButton(onPressed: null, child: Text('answer two')),
-            ElevatedButton(onPressed: null, child: Text('answer three')),
-            ElevatedButton(onPressed: null, child: Text('answer four')),
+            ElevatedButton(
+              onPressed: answerQuestion,
+              child: Text('answer one'),
+            ),
+            ElevatedButton(
+              onPressed: () => print("answer two chosen"),
+              child: Text('answer two'),
+            ),
+            ElevatedButton(
+              onPressed: () => print("answer three chosen"),
+              child: Text('answer three'),
+            ),
+            ElevatedButton(
+              onPressed: () => print("answer four chosen"),
+              child: Text('answer four'),
+            ),
           ],
         ),
       ),
